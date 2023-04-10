5be5900350fa69c5d89ee0515422158db2533bad
psw <151120093@smail.nju.edu.cn>
2023-04-10 10:26:57 +0800

    fix bug

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 from cylp.cy import CyClpSimplex
 import os
+from datetime import datetime
 
 
 if __name__ == "__main__":
@@ -11,20 +12,33 @@ if __name__ == "__main__":
     # round(s.objectiveValue, 3)
 
     from cylp.cy import CyClpSimplex
-    from cylp.py.pivots import DantzigPivot, PositiveEdgePivot, LIFOPivot, MostFrequentPivot, PositiveEdgeWolfePivot, WolfePivot
+    from cylp.py.pivots import DantzigPivot, PositiveEdgePivot, LIFOPivot, MostFrequentPivot, PositiveEdgeWolfePivot, \
+        WolfePivot
+
     # Get the path to a sample mps file
 
+    # prefix is output + time stamp as folder name
+    prefix = f"output/{datetime.now().strftime('%Y.%m.%d_%H_%M_%S')}"
     netlib = "cylp/input/netlib"
-    for mps in os.listdir(netlib):
+    for mps in sorted(os.listdir(netlib)):
         if ".mps" not in mps:
             print("Skipping", mps)
             continue
-        for pivot in [DantzigPivot, PositiveEdgePivot, LIFOPivot, MostFrequentPivot, PositiveEdgeWolfePivot, WolfePivot]:
-            f = os.path.join(netlib, mps)
+
+        # FIXME: bug
+        if mps in ("d2q06c.mps", "dfl001.mps"):
+            continue
+
+        f = os.path.join(netlib, mps)
+        for pivot in [
+            DantzigPivot,
+            PositiveEdgePivot,
+            # LIFOPivot,  # too slow
+            # MostFrequentPivot,  # too slow
+        ]:
             s = CyClpSimplex()
             s.readMps(f)  # Returns 0 if OK
-            pivot = DantzigPivot(s, mps.split(".")[0])
-            # pivot = PositiveEdgePivot(s)
-            s.setPivotMethod(pivot)
+            pt = pivot(s, prefix, mps.split(".")[0])
+            s.setPivotMethod(pt)
             s.primal()
             round(s.objectiveValue, 5)
